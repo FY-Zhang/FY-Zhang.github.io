e0a85c551a23a99b96c12c886a2565e6f6d6664a
FY-Zhang <457591198@qq.com>
2018-12-26 01:39:46 +0800

    1

--- Databases/CreateInformation.sql
+++
@@ -1,7 +0,0 @@
-create table Information (
-	Name varchar(40) not null,
-	Type varchar(10) not null,
-	Episodes smallint,
-	Country varchar(10),
-	Cover varchar(100)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
