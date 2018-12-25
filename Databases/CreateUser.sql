cda4a8bd3278812e70948a2d921ba9996145433b
FY-Zhang <457591198@qq.com>
2018-12-26 01:55:17 +0800

    None

--- Databases/CreateUser.sql
+++
@@ -1,9 +0,0 @@
-create table User (
-	username varchar(30) not null,
-	password varchar(30),
-	Name varchar(40),
-	Type varchar(10),
-	Episodes smallint,
-	Country varchar(10),
-	interest varchar(100)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- Websites/submit.js
+++
@@ -1,4 +0,0 @@
-function submit() {
-	alert("I have received your question!")
-	location.reload()	//刷新页面
-}
